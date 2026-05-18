46ea967f2ad0ca92158106d7ccf32cbbaaf9c514
matthewstewart <matthewstewart@WLGIT0051>
2026-05-18 12:09:19 +0100

    Add project files.

---
+++ QAProjectDotNet6Plus/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = "Matty";
+string date = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine("Hello " + name + ", how are you on " + date + "?");
+Console.ReadLine();
+Console.WriteLine($"Hello {name}, how are you on {date}?");
+Console.ReadLine();
